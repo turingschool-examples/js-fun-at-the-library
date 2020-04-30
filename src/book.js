c37d4a9e72aa45f0d07303f8f103126282d1fa64
Casey Dallavalle <cbdallavalle@gmail.com>
2020-04-30 09:27:22 -0600

    Finish book test

--- src/book.js
+++ src/book.js
@@ -2,6 +2,29 @@ function createTitle(title) {
   return "The " + title;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function calculatePageCount(title) {
+  return title.length * 20
+}
+
+function writeBook(title, character) {
+  return {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title)
+  }
+}
+
 module.exports = {
-  createTitle
+  createTitle,
+  buildMainCharacter,
+  calculatePageCount,
+  writeBook
 }

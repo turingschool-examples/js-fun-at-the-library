857c6071593ac95d708f8cb4cd23c42df3f3ec77
Leigh Larson <leighlarson4@gmail.com>
2020-05-06 17:21:42 -0600

    Completed Warm-up Exercises

--- src/warm-up.js
+++ src/warm-up.js
@@ -1,89 +1,41 @@
-// Read the instructions for each of the warm up exercises carefully. This may
-// seem repititious or pedandtic at first, however it's very important that you
-// become comfortable with these fundamental concepts. Do not move on to
-// iteration 1 until you are confident that you've completed all of iteration 0.
-
-
 //------------------------- Variables -------------------------//
+var title = "Becoming";
+var pages = 340;
 
+pages = 340 / 4;
 
-// Initialize two new variables, "title", and "pages", and assign values to them with
-// a book's title and page count.
+var movie =;
+var sequel = `${title} Again`;
+var isFavorite = false;
+var isShortStory = pages < 25;
 
-// var title = "Harry Potter";
-// var ...
+//------------------------- Functions -------------------------//
+function quickRead() {
+  if (title.length < 1000) {
+    console.log("I could read that in a day!");
+  } else {
+    console.log("Ok, maybe I could read that in a day and a half.");
+  }
+}
 
-
-
-// Reassign (not initialize) your "page" variable from above. An editor came around and
-// condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
-
-
-
-// Initialize a new variable "movie", but leave it undefined
-
-
-
-// Create a variable named "sequel", and assign it to a be a sequel of your
-// book (made up or not!). Your sequel must make use of your "title" variable from above
-
-
-
-// Initialize a new variable "isFavorite", and assign it to a boolean value indicating
-// if the book you chose is your favorite book or not
-
-
-
-
-// Create a new variable named "isShortStory", and use a comparison operator to
-// assign a boolean value to this variable. If "pageCount" is less than 25, the
-// variable should be true, otherwise it should be false. You MUST use a
-// comparison operator.
-
-
-
-//------------------------- Conditionals -------------------------//
-
-
-// Express the following in code: If the variable title is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
-// 'Ok, maybe I could read that in a day and a half'
-
-
-
-// Express the following in code: If the variable "isFavorite" is true,
-// log the statement 'My favorite book is [title]!'. Otherwise, log the
-// statement 'I recently read the [title] book'. In either case, the value stored in
-// the "title" variable should be included in the logged statement.
-
-
+function faveBook() {
+  if (isFavorite === true) {
+    console.log("My favorite book is ${title}!");
+  } else {
+    console.log("I recently read the ${title} book.");
+  }
+}
 
 //------------------------- Arrays -------------------------//
 
-
-// Create a new variable "books" and assign it to an Array of 5 different
-// book titles, represented by their names as Strings.
-
+var books = ["Romeo & Juliet", "Origin of the Species", "Siddartha", "Wizard of Oz", "Harry Potter"];
+books[2];
 
 
-// Write the code to access the 3rd element of the Array, using bracket
-// notation.
-
-
-// Create a new variable "belowTwoHundred" and assign it to an Array of 12
-// different numbers below two hundred.
-
-
-// Write the code to access the number cllosest to two hundred, using bracket notation.
-
+var belowTwoHundred = [2, 4, 6, 8, 100, 150, 165, 170, 178, 189, 190, 199];
+belowTwoHundred[11];
 
 //------------------------- Objects -------------------------//
 
-// Create a new variable "dictionary" and assign to it an object literal with two
-// keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
-// the value for hasThesaurus should be false
-
-
-// Write the code to add the key of pageCount, with the value of 720. Use dot
-// notation to do this. DO NOT modify your code from above.
+var dictionary = {name: "Webster's", hasThesaurus: false};
+dictionary.pageCount = 720;

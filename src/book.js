f267f6262bbabde1028d546793162ea7f8a7beef
Leigh Larson <leighlarson4@gmail.com>
2020-05-06 18:35:40 -0600

    Completed book test exercises

--- src/book.js
+++ src/book.js
@@ -1,12 +1,37 @@
-function createTitle() {
-  
+function createTitle(bookTitle) {
+  return `The ${bookTitle}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name, age, pronouns};
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review)
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  var bookPageCount = bookTitle.length * 20;
+  return bookPageCount
+}
+
+function writeBook(title, mainCharacter, genre) {
+  var book = {title, mainCharacter, genre};
+  book.pageCount = calculatePageCount(title);
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount = (book.pageCount * .75);
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }

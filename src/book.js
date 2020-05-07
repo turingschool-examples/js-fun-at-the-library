162600a57a70ed7510e7e1ac726440aa28c30e5a
Suzie Q <suzieqwolf@gmail.com>
2020-05-06 22:08:54 -0600

    Adds code for crteTtle, bldMain & saveRev to book.

--- src/book.js
+++ src/book.js
@@ -1,12 +1,33 @@
-function createTitle() {
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name, age, pronouns,
+  }
+}
+
+function saveReview(review, reviews) {
+  reviews.includes(review) ? reviews : reviews.push(review);
+  //reviews.push(review);
+}
+
+function calculatePageCount() {
   
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
   // writeBook: writeBook,
   // editBook: editBook
 }
+
+
+
+// //  #1
+
+// //  npm test test/book-test.js
--- test/book-test.js
+++ test/book-test.js
@@ -1,23 +1,24 @@
 var assert = require("chai").assert;
 var createTitle = require("../src/book.js").createTitle;
 var buildMainCharacter = require("../src/book.js").buildMainCharacter;
+var saveReview = require("../src/book.js").saveReview;
 var calculatePageCount = require("../src/book.js").calculatePageCount;
 var writeBook = require("../src/book.js").writeBook;
 var editBook = require("../src/book.js").editBook;
 
 describe("book.js", function () {
   describe("createTitle", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(createTitle);
     });
   
-    it.skip("should take in a title and return a modified title", function () {
+    it("should take in a title and return a modified title", function () {
       var bookIdea = createTitle("Storm's Awakening");
   
       assert.equal(bookIdea, "The Storm's Awakening");
     });
   
-    it.skip("should be able to create many modified titles", function () {
+    it("should be able to create many modified titles", function () {
       var sushiTitle = createTitle("Dancing Sushi");
       var dragonTitle = createTitle("Dragon in the Summer");
       var ghostTitle = createTitle("Teenage Ghoul");
@@ -29,11 +30,11 @@ describe("book.js", function () {
   });
 
   describe("buildMainCharacter", function() {
-    it.skip("should be a function (comment the function back into the module.exports object in book.js)", function () {
+    it("should be a function (comment the function back into the module.exports object in book.js)", function () {
       assert.isFunction(buildMainCharacter);
     });
   
-    it.skip("should create a character object", function () {
+    it("should create a character object", function () {
       var ghoulCharacter = buildMainCharacter("Vassya", 16, "she/her");
   
       assert.equal(ghoulCharacter.name, "Vassya");
@@ -43,11 +44,11 @@ describe("book.js", function () {
   });
 
   describe("saveReview", function() {
-    it.skip("should be a function (comment the function back into the module.exports object in book.js)", function () {
+    it("should be a function (comment the function back into the module.exports object in book.js)", function () {
       assert.isFunction(saveReview);
     });
 
-    it.skip("should be able to add reviews to an array", function() {
+    it("should be able to add reviews to an array", function() {
       var reviews = [];
 
       saveReview("An astounding success", reviews);
@@ -56,7 +57,7 @@ describe("book.js", function () {
       assert.deepEqual(reviews, ["An astounding success"]);
     });
 
-    it.skip("should be able to add reviews to an array that already contains reviews", function() {
+    it("should be able to add reviews to an array that already contains reviews", function() {
       var reviews = ["You won't be able to put it down"];
 
       saveReview("A page turner!", reviews);
@@ -66,7 +67,7 @@ describe("book.js", function () {
       assert.deepEqual(reviews, ["You won't be able to put it down", "A page turner!", "An instant classic!"]);
     });
 
-    it.skip("should only add unique reviews", function() {
+    it("should only add unique reviews", function() {
       var reviews = [];
 
       saveReview("I want everyone to read this book!", reviews);
@@ -79,7 +80,7 @@ describe("book.js", function () {
   });
 
   describe("calculatePageCount", function() {
-    it.skip("should be a function (comment the function back into the module.exports object in book.js)", function () {
+    it("should be a function (comment the function back into the module.exports object in book.js)", function () {
       assert.isFunction(calculatePageCount);
     });
   

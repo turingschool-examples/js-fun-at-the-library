002c29357128d27913145d53c462f359eea55dad
Suzie Q <suzieqwolf@gmail.com>
2020-05-07 22:50:56 -0600

    Adds function createLibrary to Library.js file.

--- src/Library.js
+++ src/Library.js
@@ -1,8 +1,23 @@
+function createLibrary(name) {
+  return {
+    libraryName: name, 
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+}
+
+function addBook(libraryName, {book}) {
+  libraryName.shelves.genre.push({book})
+
+}
 
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
+  createLibrary: createLibrary,
+  addBook: addBook,
   // checkoutBook: checkoutBook
 };
 
--- test/Library-test.js
+++ test/Library-test.js
@@ -6,29 +6,29 @@ var addBook = require("../src/Library.js").addBook;
 
 describe("library.js", function() {
   describe("createLibrary", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(createLibrary);
     });
   
-    it.skip("should have a name", function() {
+    it("should have a name", function() {
       var denverLibrary = createLibrary("Denver Public Library");
   
     assert.equal(denverLibrary.name, "Denver Public Library");
     });
   
-    it.skip("should be able to have a different name", function() {
+    it("should be able to have a different name", function() {
       var goldenLibrary = createLibrary("Golden Public Library");
   
       assert.equal(goldenLibrary.name, "Golden Public Library");
     });
   
-    it.skip("should have shelves", function() {
+    it("should have shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
   
       assert.isObject(denverLibrary.shelves);
     });
   
-    it.skip("should have several different types of shelves", function() {
+    it("should have several different types of shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
   
       assert.deepEqual(denverLibrary.shelves.fantasy, []);
@@ -38,7 +38,7 @@ describe("library.js", function() {
   });
   
   describe("addBook", function() {
-    it.skip("should add book to the fantasy shelf", function() {
+    it("should add book to the fantasy shelf", function() {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",

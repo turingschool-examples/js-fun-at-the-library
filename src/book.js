f4996e7f0f7cec44c778e4affaf69ff423d7b4bf
Elle Li <yiran.li@hotmail.com>
2020-08-19 22:22:56 -0600

    Finished Testing

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(name) {
+  return `The ${name}`
 }
+
+function buildMainCharacter(name,age,pronouns){
+  return {
+    name:name,
+    age:age,
+    pronouns:pronouns,
+  }
+}
+
+function saveReview(rev,reviews){
+  if (reviews.includes(rev)==false) {
+    reviews.push(rev)
+    }
+}
+
+function calculatePageCount(title) {  
+  return (title.length*20)
+}
+
+function writeBook(title,character,genre) {
+  return {
+    title:title,
+    mainCharacter:character,
+    pageCount:calculatePageCount(title),
+    genre:genre,
+  }
+}
+
+function editBook(book){
+
+  book.pageCount = book.pageCount *0.75
+}
+
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }

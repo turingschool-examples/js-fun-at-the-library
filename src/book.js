35b6e69ddc508656777049fc82ab756edd4138ed
Cameron Romo <cromo1225@gmail.com>
2020-08-20 15:58:36 -0600

    Add buildMainCharacter()

--- src/book.js
+++ src/book.js
@@ -2,9 +2,18 @@ function createTitle(title) {
   return `The ${title}`;
 };
 
+function buildMainCharacter() {
+  var ghoulCharacter = {
+    name: "Vassya",
+    age: 16,
+    pronouns: "she/her"
+  };
+  return ghoulCharacter;
+};
+
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
+  buildMainCharacter: buildMainCharacter,
   // saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
   // writeBook: writeBook,

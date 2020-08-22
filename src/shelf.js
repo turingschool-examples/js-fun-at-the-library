b8f030625abd1c43d819a3e4d917b018b4907567
Cameron Romo <cromo1225@gmail.com>
2020-08-22 14:37:36 -0600

    Add listTitles()

--- src/shelf.js
+++ src/shelf.js
@@ -6,10 +6,25 @@ function shelfBook(book, sciFiShelf) {
   }
 };
 
+function unshelfBook(name, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (name === sciFiShelf[i].title) {
+      return sciFiShelf.splice(i, 1);
+    }
+  }
+};
+
+function listTitles(fantasyShelf) {
+  var nameArray = [];
+  for (var i = 0; i < fantasyShelf.length; i++) {
+    nameArray.push(fantasyShelf[i].title);
+    }
+  return nameArray.join(", ");
+};
 
 module.exports = {
   shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
   // searchShelf: searchShelf
 };

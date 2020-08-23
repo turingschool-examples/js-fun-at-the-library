96d21f429d8905a4fc8c284096afc8241b1af6ab
Cameron Romo <cromo1225@gmail.com>
2020-08-23 15:25:46 -0600

    Unskip tests

--- test/Library-test.js
+++ test/Library-test.js
@@ -5,39 +5,39 @@ var checkoutBook = require("../src/library.js").checkoutBook;
 
 describe("library.js", function() {
   describe("createLibrary", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(createLibrary);
     });
-  
-    it.skip("should have a name", function() {
+
+    it("should have a name", function() {
       var denverLibrary = createLibrary("Denver Public Library");
-  
+
     assert.equal(denverLibrary.name, "Denver Public Library");
     });
-  
-    it.skip("should be able to have a different name", function() {
+
+    it("should be able to have a different name", function() {
       var goldenLibrary = createLibrary("Golden Public Library");
-  
+
       assert.equal(goldenLibrary.name, "Golden Public Library");
     });
-  
-    it.skip("should have shelves", function() {
+
+    it("should have shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
-  
+
       assert.isObject(denverLibrary.shelves);
     });
-  
-    it.skip("should have several different types of shelves", function() {
+
+    it("should have several different types of shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
-  
+
       assert.deepEqual(denverLibrary.shelves.fantasy, []);
       assert.deepEqual(denverLibrary.shelves.fiction, []);
       assert.deepEqual(denverLibrary.shelves.nonFiction, []);
     });
   });
-  
+
   describe("addBook", function() {
-    it.skip("should add book to the fantasy shelf", function() {
+    it("should add book to the fantasy shelf", function() {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
@@ -45,13 +45,13 @@ describe("library.js", function() {
         pageCount: 418,
         genre: "fantasy"
       }
-  
+
       addBook(denverLibrary, dracula);
-  
+
       assert.equal(denverLibrary.shelves.fantasy[0], dracula);
     });
 
-    it.skip("should add books to the correct shelves automatically", function() {
+    it("should add books to the correct shelves automatically", function() {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
@@ -73,9 +73,9 @@ describe("library.js", function() {
       assert.equal(denverLibrary.shelves.nonFiction[0], paleBlueDot);
     });
   });
-  
+
   describe("checkoutBook", function() {
-    it.skip("should unshelf a book to check out a book to a patron", function() {
+    it("should unshelf a book to check out a book to a patron", function() {
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
@@ -95,22 +95,22 @@ describe("library.js", function() {
         genre: "fiction"
       }
       var denverLibrary = createLibrary("Denver Public Library");
-  
+
       addBook(denverLibrary, dracula);
       addBook(denverLibrary, bornACrime);
       addBook(denverLibrary, prideAndPrejudice);
-  
+
       var result = checkoutBook(denverLibrary, "Pride and Prejudice");
-  
+
       assert.deepEqual(denverLibrary.shelves, {fantasy: [dracula], fiction: [], nonFiction: [bornACrime]});;
       assert.equal(result, "You have now checked out Pride and Prejudice from the Denver Public Library")
     });
-    
-    it.skip("should only checkout a book if the book is on the shelves", function() {
+
+    it("should only checkout a book if the book is on the shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
-          
+
       var error = checkoutBook(denverLibrary, "The Fifth Season");
-      
+
       assert.equal(error, "Sorry, there are currently no copies of The Fifth Season available at the Denver Public Library");
     });
   });

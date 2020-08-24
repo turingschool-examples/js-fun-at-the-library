1b34de808a8712f9b98d2ae356eab57831230cb9
Cameron Romo <cromo1225@gmail.com>
2020-08-24 13:44:29 -0600

    Refactor checkOutBook()

--- src/Library.js
+++ src/Library.js
@@ -25,31 +25,6 @@ function checkoutBook(libObj, bookName) {
   return `Sorry, there are currently no copies of ${bookName} available at the ${libObj.name}`
 };
 
-// function checkoutBook(library, bookName) {
-//   for (var i = 0; i < library.shelves.fiction.length; i++) {
-//     if (bookName == library.shelves.fiction[i].title) {
-//       library.shelves.fiction.splice(i, 1);
-//       return `You have now checked out ${bookName} from the ${library.name}`;
-//     }
-//   }
-//
-//   for (var i = 0; i < library.shelves.fantasy.length; i++) {
-//     if (bookName == library.shelves.fantasy[i].title) {
-//       library.shelves.fantasy.splice(i, 1);
-//       return `You have now checked out ${bookName} from the ${library.name}`
-//     }
-//   }
-//
-//   for (var i = 0; i < library.shelves.nonFiction.length; i++) {
-//     if (bookName == library.shelves.nonFiction[i].title) {
-//       library.shelves.nonFiction.splice(i, 1);
-//       return `You have now checked out ${bookName} from the ${library.name}`
-//     }
-//   }
-//
-//   return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`;
-// };
-
 module.exports = {
   createLibrary: createLibrary,
   addBook: addBook,

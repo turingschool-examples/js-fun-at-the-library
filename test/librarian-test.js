8d06896f7def9e73bfc81d22d6b284772a19c5e9
Cameron Romo <cromo1225@gmail.com>
2020-08-24 15:06:25 -0600

    Unskip tests

--- test/librarian-test.js
+++ test/librarian-test.js
@@ -64,7 +64,7 @@ describe('Librarian (class)', function() {
     assert.equal(bookConfirmation, "Yes, we have The Fifth Season");
   });
 
-  it.skip('should say if requested book is not found', function() {
+  it('should say if requested book is not found', function() {
     var library = createLibrary("Aurora Public Library");
     var librarian = new Librarian("Casey", library);
     var book = { title: 'The Fifth Season', genre: 'fantasy' }
@@ -76,7 +76,7 @@ describe('Librarian (class)', function() {
     assert.equal(bookConfirmation, "Sorry, we do not have Five Brief Lessons in Physics");
   });
 
-  it.skip('should checkout the book if found', function() {
+  it('should checkout the book if found', function() {
     var library = createLibrary("Aurora Public Library");
     var librarian = new Librarian("Casey", library);
     var book = { title: 'The Fifth Season', genre: 'fantasy' }
@@ -88,7 +88,7 @@ describe('Librarian (class)', function() {
     assert.deepEqual(librarian.library.shelves.fantasy, []);
   });
 
-  it.skip('calculateLateFee should charge the customer a fee of 0.25 per day the book is late, rounded up to the closest whole number (integer)', function() {
+  it('calculateLateFee should charge the customer a fee of 0.25 per day the book is late, rounded up to the closest whole number (integer)', function() {
     var library = createLibrary("Aurora Public Library");
     var librarian = new Librarian("Casey", library);
     var threeDaysLate = librarian.calculateLateFee(3);

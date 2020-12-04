7b0e500c5fe86b900d82c1488d5d8db2ac0d2699
Aaron Foucheaux <afoucheaux@gmail.com>
2020-12-03 21:36:25 -0600

    Complete books.js & book test passing. Still need to clean up/refactor code

--- src/book.js
+++ src/book.js
@@ -1,12 +1,52 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns}
+}
+
+function saveReview(rev, reviews) {
+  if (reviews.length === 0) {
+        reviews.push(rev);
+  } else {
+    for (i = 0; i < reviews.length; i++) {
+      if (rev === reviews[i]) {
+        var matched = true;
+      }
+    }
+    if (matched !== true) {
+      reviews.push(rev);
+    }
+  }
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.split("").length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+  return book;
+}
+
+function editBook(book){
+  book.pageCount = book.pageCount * .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }

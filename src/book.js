6febf03d62ec59af95b2ec0fa531769a455aa008
Steven Mancine <stevenrmancine@gmail.com>
2020-12-05 17:32:57 -0700

    Add rough draft 3 files

--- src/book.js
+++ src/book.js
@@ -1,12 +1,60 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  var newCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return newCharacter;
+}
+
+// buildMainCharacter is the function, parameters name, age, pronouns. newCharacters is the object.
+// when running the test we are looking to access values inside of the object, this is an empty object
+var reviews = [];
+
+function saveReview(text, reviews) {
+  if (reviews.length === 0) {
+    reviews[0] = text;
+  } else {
+      for (var i = 0; i < reviews.length; i++) {
+        if(reviews.indexOf(text) === -1) {
+        reviews.push(text);
+      }
+    }
+  }
+};
+//.push - the array method to add element to the end of an array
+//.indexOf - the method to check the index number of array element. If no element exist it will return -1
+
+
+
+/////////////////////////////////////////////////////
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+///////////////////////////////////////////////////////////
+function writeBook(bookTitle, bookCharacter, genre) {
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre,
+  };
+};
+/////////////////////////////////////////////////////////////////
+function editBook(bookTitle) {
+  bookTitle.pageCount = bookTitle.pageCount * .75;
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
--- src/warm-up.js
+++ src/warm-up.js
@@ -12,27 +12,30 @@
 
 // var title = "Harry Potter";
 // var ...
-
+var title = "Slaughter House Five";
+var pages = 116;
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
-
+// of what it currently is
+pages /= 4;
+console.log(pages);
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie;
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
+var sequel = title + ": The Wonder Years";
 
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
-
+var isFavorite = false;
 
 
 
@@ -41,15 +44,29 @@
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+var isShortStory;
+
+if (pages < 25) {
+  isShortStory = true;
+} else {
+  isShortStory = false;
+}
+console.log(isShortStory);
+
+
 
 
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
-
+if (pages < 1000) {
+  console.log("I could read that in a day!");
+} else {
+  console.log("Ok, maybe I could read that in a day and a half");
+}
 
 
 // Express the following in code: If the variable "isFavorite" is true,
@@ -64,25 +81,30 @@
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
-
+var books = ["War and Peace", "How to Change the World", "Born to Run", "The Road", "Conversations With Youself"];
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+console.log(books[2]);
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
-
+var belowTwoHundred = [12, 4, 3, 77, 23, 6, 75, 36, 99, 198, 0 , 44];
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+console.log(belowTwoHundred[4]);
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable "dictionary" and assign to it an object literal with two
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
-
-
+var dictionary = {
+  name: "Webster's",
+  hasThesaurus: false,
+}
+console.log(dictionary);
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+dictionary.pageCount = 720;
+console.log(dictionary);

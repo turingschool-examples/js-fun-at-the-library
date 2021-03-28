4e057be9780225050c371fcc1451dfa8c25c200f
William McGuire <wmcguire18@gmail.com>
2021-03-28 19:57:50 -0400

    Initial commit

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,48 @@
+//searchShelf
+  // - should tell us if a title is on the shelf
+  // - should tell us if a title is on a different shelf
+
+  function shelfBook(book, shelf){
+    if (shelf.length < 3) {
+      shelf.unshift(book);
+      }
+};
+
+function unshelfBook(book, shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      shelf.splice(i, 1);
+    }
+  }
+};
+
+function listTitles(shelf) {
+  var titleList = "";
+  for (i = 0; i < shelf.length; i++) {
+    if (i < shelf.length - 1){
+    titleList += shelf[i].title + ", ";
+  }else {
+    titleList += shelf[i].title
+  }
+  }
+    return titleList;
+};
+
+function searchShelf(shelf, title) {
+  var onShelf = false;
+for (i = 0; i < shelf.length; i++) {
+  if (shelf[i].title === title) {
+    onShelf = true;
+}
+}
+return onShelf
+};
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };

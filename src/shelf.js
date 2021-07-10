f8764b1e934537639e7c0675e34d2bd183e8747d
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-07-09 20:14:50 -0600

    Refactor searchShelf function

--- src/shelf.js
+++ src/shelf.js
@@ -5,7 +5,7 @@ function shelfBook(book, shelf) {
      if (shelf.length < 3) {
       shelf.unshift(book);
      }
-}
+};
 //Function that remove a book from the array shelf.
 function unshelfBook(bookName, shelf) {
       for (var i = 0; i < shelf.length; i++) {
@@ -13,7 +13,7 @@ function unshelfBook(bookName, shelf) {
           shelf.splice(i, 1);
         }
       }
-}
+};
 
 //Function that lists a string with the titles of the books.
 function listTitles(listOfBooks) {
@@ -24,22 +24,20 @@ function listTitles(listOfBooks) {
     }
     listOfTitles = listOfTitles.slice(0, listOfTitles.length - 2)
     return listOfTitles;
-}
+};
 
 // Function that search on the shelf if title exists staring at the end of the shelf array.
-//Ask tutor how to do it starting from the begginig.
 function searchShelf(shelf, bookTitle) {
-      var isOnShelf;
-      for (var i = shelf.length - 1; i >= 0; i--) {
-      if (shelf[i].title === bookTitle) {
-        isOnShelf = true;
-        return isOnShelf;
-      } else if (shelf[i].title !== bookTitle) {
-        isOnShelf = false;
-        return isOnShelf;
-      }
-      }
-}
+      var isOnShelf = false;
+      for (var i = 0; i < shelf.length; i++) {
+       if (shelf[i].title === bookTitle) {
+          return isOnShelf = true;
+      // } else if (shelf[i].title === bookTitle){
+      //   isOnShelf = true;
+      };
+    };
+      return isOnShelf ;
+};
 
 module.exports = {
   shelfBook,

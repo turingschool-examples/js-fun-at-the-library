81734ece21fc3ea603c132cdef9d8588a5b44d07
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-07-10 19:48:29 -0600

    Add functions on librarian.js

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,34 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
+
+  greetPatron(patron, isMorning) {
+    if (isMorning === true) {
+      return `Good morning, ${patron}!`
+    };
+    return `Hello, ${patron}!`;
+  };
+
+  findBook(bookToFind) {
+    var isOnLibrary = false;
+    for (var key in this.library.shelves) {
+        for (var i = 0; i < this.library.shelves[key].length; i++) {
+          if (bookToFind === this.library.shelves[key][i].title) {
+            this.library.shelves[key].splice(i, 1);
+            return `Yes, we have ${bookToFind}`; 
+          };
+        };
+        return `Sorry, we do not have ${bookToFind}`;
+    };
+  };
+
+  calculateLateFee(numberOfDaysLate) {
+    var lateFee = numberOfDaysLate * 0.25;
+    return Math.ceil(lateFee);
   }
-}
+
+  };
 
 module.exports = Librarian;

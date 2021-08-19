185991facd7a0cc45dcc2a423e09911d18370ad3
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-08-19 16:59:36 -0600

    Change to arrow functions

--- src/book.js
+++ src/book.js
@@ -1,48 +1,73 @@
 
-// Function to add "The " at the beginning of a title.
-function createTitle(title) {
-    var modifiedTittle = "The " + title;
-    return modifiedTittle;
-};
-// Function to build a Main Character object with 3 properties
-function buildMainCharacter(name, age, pronouns) {
-      var newMainCharacter = {
-        name: name,
-        age: age,
-        pronouns: pronouns,
-      };
-      return newMainCharacter;
+//Function to add "The " at the beginning of a title.
+// function createTitle(title) {
+//     var modifiedTittle = "The " + title;
+//     return modifiedTittle;
+// };
 
-};
+// Arrow function
+
+createTitle = title => 'The ' + title;
+
+// Function to build a Main Character object with 3 properties.
+// function buildMainCharacter(name, age, pronouns) {
+//       var newMainCharacter = {
+//         name: name,
+//         age: age,
+//         pronouns: pronouns,
+//       };
+//       return newMainCharacter;
+
+// };
+
+buildMainCharacter = (name, age, pronouns) => ({ name, age, pronouns });
+
 // Function to save new reviews in an array, without repeating any review.
-function saveReview(newReview, reviews) {
-    for (var i = 0; i < reviews.length; i++) {
-      if (reviews[i] === newReview) {
-         return reviews
-      }
+// function saveReview(newReview, reviews) {
+//     for (var i = 0; i < reviews.length; i++) {
+//       if (reviews[i] === newReview) {
+//          return reviews
+//       }
         
-    };
-    return reviews.push(newReview);
-};
+//     };
+//     return reviews.push(newReview);
+// };
+
+saveReview = (newReview, reviews) => !reviews.includes(newReview) ? reviews.push(newReview): reviews
+
+
 // Function to add 20 pages per letter in the title.
-function calculatePageCount(title) {
-    var pageCount = title.length * 20;
-    return pageCount;
-};
+// function calculatePageCount(title) {
+//     var pageCount = title.length * 20;
+//     return pageCount;
+// };
+
+calculatePageCount = title => title.length * 20;
+
 // Function that creates an book object.
-function writeBook(bookTitle, bookCharacter, genre) {
-    var newBook = {
-      title: bookTitle,
-      mainCharacter: bookCharacter,
-      genre: genre,
-      pageCount: calculatePageCount(bookTitle),
-    };
-    return newBook;
-};
+// function writeBook(bookTitle, bookCharacter, genre) {
+//     var newBook = {
+//       title: bookTitle,
+//       mainCharacter: bookCharacter,
+//       genre: genre,
+//       pageCount: calculatePageCount(bookTitle),
+//     };
+//     return newBook;
+// };
+
+writeBook = (bookTitle, bookCharacter, genre) => ({
+  title: bookTitle,
+  mainCharacter: bookCharacter,
+  genre,
+  pageCount: calculatePageCount(bookTitle)
+});
+
 // Function that decrease by 1/4 the pageCount.
-function editBook(newBook) {
-    newBook.pageCount = newBook.pageCount * 0.75;
-  };
+// function editBook(newBook) {
+//     newBook.pageCount = newBook.pageCount * 0.75;
+//   };
+
+editBook = newBook => newBook.pageCount = newBook.pageCount * 0.75;
 
 
 module.exports = {

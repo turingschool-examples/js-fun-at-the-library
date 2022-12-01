7a883889b266592d3f8176b04e3339a60ed55e24
Michael Lobato <mike.lobato.wft@gmail.com>
2022-12-01 10:15:48 -0800

    Adds; page count function to decrease volume by a fourth

--- src/book.js
+++ src/book.js
@@ -20,12 +20,24 @@ function calculatePageCount(title){
   return (title.length * 20)
 }
 
+function writeBook(title, mainCharacter, genre){
+  return book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+}
+
+function editBook(book){
+  return book.pageCount = book.pageCount * .75 
+}
 
 module.exports = {
   createTitle,
   buildMainCharacter,
   saveReview,
   calculatePageCount,
-  // writeBook,
-  // editBook
+  writeBook,
+  editBook
 }

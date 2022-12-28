19be10448e796af25457c606971c7e15eb89365a
Kim G <xiaole.guo@yahoo.com>
2022-12-28 14:49:37 -0700

    complete iteration 3

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,28 @@
 
+function createLibrary(name){
+  library = {name: name, shelves: {fantasy:[], fiction: [], nonFiction:[]}}
+  return library
+}
 
+function addBook(library, book) {
+  for (let key in library.shelves){
+    if (book.genre == key) {
+      library.shelves[key].push(book)
+    }
+  }
+}
+
+function checkoutBook(library, bookname, genre) {
+  for (let i=0; i < library.shelves[genre].length; i++){
+    if (library.shelves[genre][i].title == bookname) {
+      library.shelves[genre].splice(i, 1)
+      return `You have now checked out ${bookname} from the ${library.name}`
+    }
+  }
+  return `Sorry, there are currently no copies of ${bookname} available at the ${library.name}`
+}
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
--- test/Library-test.js
+++ test/Library-test.js
@@ -8,31 +8,30 @@ var {
 
 describe("library.js", function() {
   describe("createLibrary", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(createLibrary);
     });
 
-    it.skip("should have a name", function() {
+    it("should have a name", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
     assert.equal(denverLibrary.name, "Denver Public Library");
     });
 
-    it.skip("should be able to have a different name", function() {
+    it("should be able to have a different name", function() {
       var goldenLibrary = createLibrary("Golden Public Library");
 
       assert.equal(goldenLibrary.name, "Golden Public Library");
     });
 
-    it.skip("should have shelves", function() {
+    it("should have shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
       assert.isObject(denverLibrary.shelves);
     });
 
-    it.skip("should have several different types of shelves", function() {
+    it("should have several different types of shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
-
       assert.deepEqual(denverLibrary.shelves.fantasy, []);
       assert.deepEqual(denverLibrary.shelves.fiction, []);
       assert.deepEqual(denverLibrary.shelves.nonFiction, []);
@@ -40,7 +39,7 @@ describe("library.js", function() {
   });
 
   describe("addBook", function() {
-    it.skip("should add book to the fantasy shelf", function() {
+    it("should add book to the fantasy shelf", function() {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
@@ -54,7 +53,7 @@ describe("library.js", function() {
       assert.equal(denverLibrary.shelves.fantasy[0], dracula);
     });
 
-    it.skip("should add books to the correct shelves automatically", function() {
+    it("should add books to the correct shelves automatically", function() {
       var denverLibrary = createLibrary("Denver Public Library");
       var dracula = {
         title: "Dracula",
@@ -78,7 +77,7 @@ describe("library.js", function() {
   });
 
   describe("checkoutBook", function() {
-    it.skip("should unshelf a book to check out a book to a patron", function() {
+    it("should unshelf a book to check out a book to a patron", function() {
       var dracula = {
         title: "Dracula",
         mainCharacter: { name: "Count Dracula", age: undefined, pronouns: "he/him" },
@@ -114,7 +113,7 @@ describe("library.js", function() {
       assert.equal(result2, "You have now checked out Born a Crime from the Denver Public Library")
     });
 
-    it.skip("should only checkout a book if the book is on the shelves", function() {
+    it("should only checkout a book if the book is on the shelves", function() {
       var denverLibrary = createLibrary("Denver Public Library");
 
       var error1 = checkoutBook(denverLibrary, "The Fifth Season", "fantasy");

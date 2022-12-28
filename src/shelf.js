dfd8a1549180dcf39c67a9771c62cbd80ac18284
Kim G <xiaole.guo@yahoo.com>
2022-12-28 14:32:12 -0700

    iteration 2 complete

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,37 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+  shelf.unshift(book)
+}
+}
+function unshelfBook(bookname, shelf) {
+  for (let i=0; i < shelf.length; i++) {
+    if (bookname == shelf[i].title) {
+      shelf.splice(i, 1)
+    }
+  }
+}
+function listTitles(shelf) {
+  titles = ''
+  for (let i=0; i < shelf.length; i++) {
+      titles = titles + shelf[i].title + ', '
+    }
+  titles = titles.slice(0, -2)
+  return titles
+}
+function searchShelf(shelf, title) {
+  titles = listTitles(shelf)
+  if (titles.includes(title)) {
+    return true
+  }
+  else {
+    return false
+  }
 
+
+}
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
--- test/shelf-test.js
+++ test/shelf-test.js
@@ -9,11 +9,11 @@ var {
 
 describe("shelf.js", function() {
   describe("shelfBook", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(shelfBook);
     });
 
-    it.skip("should add books to a specific shelf", function() {
+    it("should add books to a specific shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -35,7 +35,7 @@ describe("shelf.js", function() {
       assert.equal(sciFiShelf[1], hyperion);
     });
 
-    it.skip("should add another book to the shelf", function() {
+    it("should add another book to the shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -64,7 +64,7 @@ describe("shelf.js", function() {
       assert.equal(sciFiShelf[2], hyperion);
     });
 
-    it.skip("shelves can only hold a certain amount of books", function () {
+    it("shelves can only hold a certain amount of books", function () {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -104,11 +104,11 @@ describe("shelf.js", function() {
   });
 
   describe("unshelfBook", function() {
-    it.skip("should be a function", function() {
+    it("should be a function", function() {
       assert.isFunction(unshelfBook);
     });
 
-    it.skip("should remove a book by name", function() {
+    it("should remove a book by name", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -140,7 +140,7 @@ describe("shelf.js", function() {
       assert.deepEqual(sciFiShelf, [endersGame]);
     });
 
-    it.skip("should remove a book on a different shelf by name", function() {
+    it("should remove a book on a different shelf by name", function() {
       var bossypants = {
         title: "Bossypants",
         mainCharacter: { name: "Tina Fey", age: 50, pronouns: "she/her" },
@@ -168,11 +168,11 @@ describe("shelf.js", function() {
   });
 
   describe("listTitles", function () {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(listTitles);
     });
 
-    it.skip("should create a list of all the titles on a shelf", function () {
+    it("should create a list of all the titles on a shelf", function () {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -200,7 +200,7 @@ describe("shelf.js", function() {
   });
 
   describe("searchShelf", function() {
-    it.skip("should tell us if a title is on the shelf", function() {
+    it("should tell us if a title is on the shelf", function() {
       var hyperion = {
         title: "Hyperion",
         mainCharacter: { name: "The Shrike", age: null, pronouns: "they" },
@@ -219,7 +219,7 @@ describe("shelf.js", function() {
       assert.equal(searchShelf(sciFiShelf, "The Fifth Season"), false);
     });
 
-    it.skip("should tell us if a title is on a different shelf", function() {
+    it("should tell us if a title is on a different shelf", function() {
       var bossypants = {
         title: "Bossypants",
         mainCharacter: { name: "Tina Fey", age: 50, pronouns: "she/her" },

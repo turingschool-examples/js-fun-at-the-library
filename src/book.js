d3454c9a01ee8ad57ddf285f06830a6e1bd09d74
Denzel Turambi <dturambi26@gmail.com>
2023-03-23 18:56:17 -0600

    Remove skips in first two describes

--- src/book.js
+++ src/book.js
@@ -1,10 +1,17 @@
-function createTitle() {
-  
+function createTitle(bookName) {
+  bookIdea = `The ${bookName}`;
+  return bookIdea;
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  var name = name;
+  var age = age;
+  var pronouns = pronouns;
+  return name, age, pronouns;
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
--- test/book-test.js
+++ test/book-test.js
@@ -12,17 +12,17 @@ var {
 
 describe("book.js", function () {
   describe("createTitle", function() {
-    it.skip("should be a function", function () {
+    it("should be a function", function () {
       assert.isFunction(createTitle);
     });
 
-    it.skip("should take in a title and return a modified title", function () {
+    it("should take in a title and return a modified title", function () {
       var bookIdea = createTitle("Storm's Awakening");
 
       assert.equal(bookIdea, "The Storm's Awakening");
     });
 
-    it.skip("should be able to create many modified titles", function () {
+    it("should be able to create many modified titles", function () {
       var sushiTitle = createTitle("Dancing Sushi");
       var dragonTitle = createTitle("Dragon in the Summer");
       var ghostTitle = createTitle("Teenage Ghoul");
@@ -34,11 +34,11 @@ describe("book.js", function () {
   });
 
   describe("buildMainCharacter", function() {
-    it.skip("should be a function (comment the function back into the module.exports object in book.js)", function () {
+    it("should be a function (comment the function back into the module.exports object in book.js)", function () {
       assert.isFunction(buildMainCharacter);
     });
 
-    it.skip("should create a character object", function () {
+    it("should create a character object", function () {
       var ghoulCharacter = buildMainCharacter("Vassya", 16, "she/her");
       var bakerCharacter = buildMainCharacter("Hannah", 30, "she/her");
 

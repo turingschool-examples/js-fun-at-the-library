99f6c3d7c7cf92193cb83426ac8e92e31ce0e86e
Denzel Turambi <dturambi26@gmail.com>
2023-03-23 18:01:14 -0600

    Add to objects warm up

--- src/warm-up.js
+++ src/warm-up.js
@@ -100,6 +100,14 @@ console.log(belowTwoHundred[4]);
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
 
+var dictionary = {
+    name: "Webster's",
+    hasThesaurus: false
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+dictionary.pageCount = 720;
+
+console.log(dictionary);

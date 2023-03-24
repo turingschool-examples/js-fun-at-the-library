030834d690fb4fe84776a32ddca2c71783b97763
Denzel Turambi <dturambi26@gmail.com>
2023-03-24 15:17:24 -0600

    Remove skips for saveReview

--- test/book-test.js
+++ test/book-test.js
@@ -53,11 +53,11 @@ describe("book.js", function () {
   });
 
   describe("saveReview", function() {
-    it.skip("should be a function (comment the function back into the module.exports object in book.js)", function () {
+    it("should be a function (comment the function back into the module.exports object in book.js)", function () {
       assert.isFunction(saveReview);
     });
 
-    it.skip("should be able to add reviews to an array", function() {
+    it("should be able to add reviews to an array", function() {
       var reviews = [];
 
       saveReview("An astounding success", reviews);
@@ -66,7 +66,7 @@ describe("book.js", function () {
       assert.deepEqual(reviews, ["An astounding success"]);
     });
 
-    it.skip("should be able to add reviews to an array that already contains reviews", function() {
+    it("should be able to add reviews to an array that already contains reviews", function() {
       var reviews = ["You won't be able to put it down"];
 
       saveReview("A page turner!", reviews);
@@ -76,7 +76,7 @@ describe("book.js", function () {
       assert.deepEqual(reviews, ["You won't be able to put it down", "A page turner!", "An instant classic!"]);
     });
 
-    it.skip("should only add unique reviews", function() {
+    it("should only add unique reviews", function() {
       var reviews = [];
 
       saveReview("I want everyone to read this book!", reviews);
@@ -89,7 +89,7 @@ describe("book.js", function () {
   });
 
   describe("calculatePageCount", function() {
-    it.skip("should be a function (comment the function back into the module.exports object in book.js)", function () {
+    it("should be a function (comment the function back into the module.exports object in book.js)", function () {
       assert.isFunction(calculatePageCount);
     });
 

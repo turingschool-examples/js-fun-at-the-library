58bc8c7540d4a6ea79df0265ad8af5db6aef9741
William Lavery <lavery_william@yahoo.com>
2023-07-09 14:47:11 -0400

    Initial commit

--- src/library.js
+++ src/library.js
@@ -1,8 +1,25 @@
+function createLibrary(name) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+}
+
+function addBook(library, book) {
+  var genre = book.genre
+      return library.shelves[genre].push(book)
+    }
+ 
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook,
   // takeStock
 };

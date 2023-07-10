edca7bef65aeece538c463be2c6f2f4f1e15cb47
William Lavery <lavery_william@yahoo.com>
2023-07-09 22:34:13 -0400

    Add final passing shelf functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+      return shelf
+  }
+}
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      shelf.splice(i, 1)
+    }
+  }
+  return shelf
+}
+
+function listTitles(shelf) {
+  var titles = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titles.push(shelf[i].title);
+    }
+    return titles.join(', ');
+  }
+
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+    return true
+    }
+  }
+  return false
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };

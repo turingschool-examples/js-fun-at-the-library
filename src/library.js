3712f64a41be3d28fbdff54abaed3065580724eb
William Lavery <lavery_william@yahoo.com>
2023-07-09 22:28:00 -0400

    Initial commit

--- src/library.js
+++ src/library.js
@@ -15,11 +15,30 @@ function addBook(library, book) {
       return library.shelves[genre].push(book)
     }
  
+function checkoutBook(library, title, genre) {
+  var shelves = library.shelves[genre];
+    for (var i = 0; i < shelves.length; i++) {
+      if (shelves[i].title === title) {
+        shelves.splice(i, 1);
+        return `You have now checked out ${title} from the ${library.name}.`;
+      }
+  }
+        return `Sorry, there are currently no copies of ${title} available at the ${library.name}.`;
+}
 
+function takeStock(library, genre) {
+  var shelves = library.shelves[genre]
+  var num = library.shelves.length;
+    for (var i = 0; i < shelves.length; i++) {
+      if (shelves[i].genre === genre) {
+        return `There are a total of ${num} ${genre} books at the ${library.name}.`
+      }
+  }
+}
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook,
-  // takeStock
+  checkoutBook,
+  takeStock
 };

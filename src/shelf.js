dc8070b3382fcbc50f4dafcbda2bf92d3851967c
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-12-09 03:49:37 -0700

    finish iteration 2

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book)
+  }
+  return shelf
+}
+
+function unshelfBook(bookTitle, shelf) {
+ const bookIndex = shelf.findIndex(book => book.title == bookTitle)
+ 
+ if (bookIndex != -1) {
+  shelf.splice(bookIndex, 1)
+ }
+}
+
+function listTitles(shelf) {
+  arrayTitles = []
+  shelf.forEach(book => {
+    arrayTitles.push(book.title)
+  });
+  return arrayTitles.join(", ")
+}
+
+function searchShelf(shelf, bookTitle) {
+  const bookIndex = shelf.findIndex(book => book.title == bookTitle)
+ 
+ if (bookIndex != -1) {
+  return true
+ } else {
+  return false
+ }
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
